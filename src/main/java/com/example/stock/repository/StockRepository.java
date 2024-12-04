673b6080476daa96cb611fa73dda3a32d58bfdfb
hs12 <loveanna1228@gmail.com>
2024-12-05 00:13:37 +0900

    [Section3] Optimistic Lock

--- src/main/java/com/example/stock/repository/StockRepository.java
+++ src/main/java/com/example/stock/repository/StockRepository.java
@@ -8,10 +8,9 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
-    @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select s from Stock s where s.id = :id")
-    Stock findByIdWithPessimisticLock(Long id);
     @Lock(LockModeType.OPTIMISTIC)
     @Query("select s from Stock s where s.id = :id")
     Stock findByIdWithOptimisticLock(Long id);
+
+
 }
