8f3808af4f166cd28a38b0675c8d2b0d24e435ef
hs12 <loveanna1228@gmail.com>
2024-12-05 00:15:04 +0900

    [Section1] 문제점

--- src/main/java/com/example/stock/domain/Stock.java
+++ src/main/java/com/example/stock/domain/Stock.java
@@ -19,9 +19,6 @@ public class Stock {
 
     private Long quantity;
 
-    @Version
-    private Long version;
-
     public Stock(Long productId, Long quantity) {
         this.productId = productId;
         this.quantity = quantity;
